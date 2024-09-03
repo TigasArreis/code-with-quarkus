d464b84b5fdbbdf96c8ee3c732c8606950da6264
Tiago Reis (CTW) <tiago.reis@ctw.bmwgroup.com>
2024-09-03 14:47:20 +0100

    Added endpoint to upload file

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,32 @@
 package org.acme;
 
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.jboss.resteasy.reactive.RestForm;
+import org.jboss.resteasy.reactive.multipart.FileUpload;
 
-@Path("/hello")
+@Path("/files")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from Quarkus REST";
+    }
+
+
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response uploadFile(@RestForm("file") FileUpload file) {
+        return Response
+                .status(Response.Status.OK)
+                .entity("file received: " + file.fileName())
+                .build();
     }
 }
